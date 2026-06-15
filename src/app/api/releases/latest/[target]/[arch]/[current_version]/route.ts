fef2a8a858af47f449b1bb9ceba9c23faba71906
Sabrina <sabrinachan.cs@gmail.com>
2026-06-15 16:23:11 -0700

    fix: download url

--- src/app/api/releases/latest/[target]/[arch]/[current_version]/route.ts
+++ src/app/api/releases/latest/[target]/[arch]/[current_version]/route.ts
@@ -25,7 +25,7 @@ export async function GET(
       'windows-x86_64': {
         signature:
           'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVTTXlZTlYvekZKU25XSncwM2FUMmFNYkVUTnFZMGJ2ajZCREl4UlRKQVZoYkVtYzVnazY1QTB6bVVxTE5CVVUxbTAvRXZVNGkycmpXQjN0MGgvNVUxb3lCOFNiSUpxK0FNPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzgxMjE5MTE2CWZpbGU6UG9tb3BsYW50XzAuMS4wX3g2NF9lbi1VUy5tc2kKZXhDLzVMV2JVVXU2cFFLcGo0L3RiWGVseWhPSTVVdW9hYUpnTDFRY2x2N2o4NUhXYlgzMHk2M2dxWGwycmxXU1pkaUJIZG84bVVsWHl1L2UrTlB3Q0E9PQo=',
-        url: 'http://localhost:3000/downloads/Pomoplant_0.1.0_x64_en-US.msi',
+        url: 'https://pomoplant.io/downloads/Pomoplant_0.1.0_x64_en-US.msi',
       },
     },
   });
