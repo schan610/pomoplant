b2852b36b7a24c59778d0a97582226e53f96aa3f
Sabrina <sabrinachan.cs@gmail.com>
2026-06-15 11:35:34 -0700

    feat: updater and new version

--- src/app/api/releases/latest/[target]/[arch]/[current_version]/route.ts
+++ src/app/api/releases/latest/[target]/[arch]/[current_version]/route.ts
@@ -1,4 +1,5 @@
 import { type NextRequest } from 'next/server';
+import semver from 'semver';
 
 export async function GET(
   _request: NextRequest,
@@ -7,15 +8,19 @@ export async function GET(
   const { target, arch, current_version } = await params;
   console.log(`Update: ${target} ${arch} ${current_version}`);
   console.log(current_version);
+  console.log(target);
+  const latestVersion = '0.2.0';
+  const pubDate = '2026-06-14T17:09:49+00:00';
+  const hasUpdate = semver.lt(current_version, latestVersion);
 
-  const hasUpdate = true;
   if (!hasUpdate) {
     return new Response(null, { status: 204 });
   }
+
   return Response.json({
     version: '0.2.0',
-    notes: 'HI!',
-    pub_date: '2026-06-11T17:35:24+00:00',
+    notes: 'Testing update 0.2.0',
+    pub_date: pubDate,
     platforms: {
       'windows-x86_64': {
         signature:
